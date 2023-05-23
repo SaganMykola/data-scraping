e2fdbe3767975a0700a40277aca9a2c841492bcd
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2023-05-23 12:37:20 +0300

    module

---
+++ module_2/module_2/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Module2Item(scrapy.Item):
+    url = scrapy.Field()
+    img_url = scrapy.Field()
+    store = scrapy.Field()
+    price = scrapy.Field
+
+    pass
+
---
+++ module_2/module_2/spiders/module.py
@@ -0,0 +1,38 @@
+import scrapy
+from bs4 import BeautifulSoup
+from module_2.SeleniumRequest import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from module_2.items import Module2Item
+import time
+
+
+class EkSpider(scrapy.Spider):
+    name = "ek"
+    allowed_domains = ["ek.ua"]
+    start_urls = [f"https://ek.ua/ua/list/91/"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SeleniumRequest(
+                url=url,
+                callback=self.parse,
+                wait_time=10
+            )
+
+
+def parse(self, response):
+    soup = BeautifulSoup(response.body, "html.parser")
+    ekList = soup.find(id="list_form1")
+    for ek in ekList:
+        if ek:
+            url = ek.find(name="a", class_="model-short-title").get("href")
+            img_url = ek.find(name="img").get("src")
+            store = ek.find("a", class_="model-short-title").find(string=True, recursive=False).strip()
+            price = ek.find()
+            yield Module2Item(
+                url=url,
+                img_url=[img_url],
+                store=[store],
+                price=[price]
+            )
